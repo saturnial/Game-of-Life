3df9e0161b7f2b00766a9a3ee63dcc0ec839033e
Kayvon Tehranian <ktehranian@gmail.com>
2017-01-19 18:00:27 -0500

    World can now iterate via `tick`, which follows the rules of Life

--- Game of Life/World.swift
+++ Game of Life/World.swift
@@ -4,18 +4,90 @@ protocol Renderable {
     func makeView() -> UIView
 }
 
-struct World {
+class World {
     
-    static let Dimension = 10
+    static let Dimension = 20
     
-    var cells: [Cell: UIView] = [:]
+    let cells: [Cell]
+    
+    let cellToViews: [Cell: UIView]
+    
+    let cellToNeighboringCells: [Cell: [Cell]]
     
     init() {
+        var accumulatorOfCells: [Cell] = []
+        var accumulatorOfCellsToViews: [Cell: UIView] = [:]
+        var accumualtorOfCellsToNeighboringCells: [Cell: [Cell]] = [:]
+        
         for x in 0..<World.Dimension {
             for y in 0..<World.Dimension {
-                let cell = Cell(x: x, y: y)
-                let view = cell.makeView()
-                cells[cell] = view
+                accumulatorOfCells.append(Cell(x: x, y: y))
+            }
+        }
+
+        for cell in accumulatorOfCells {
+            accumulatorOfCellsToViews[cell] = cell.makeView()
+            accumualtorOfCellsToNeighboringCells[cell] =
+                accumulatorOfCells.filter { cell.isNeighbor(of: $0) }
+        }
+
+        self.cells = accumulatorOfCells
+        self.cellToViews = accumulatorOfCellsToViews
+        self.cellToNeighboringCells = accumualtorOfCellsToNeighboringCells
+    }
+    
+    /**
+     Any live cell with fewer than two live neighbours dies, as if caused by underpopulation.
+     Any live cell with two or three live neighbours lives on to the next generation.
+     Any live cell with more than three live neighbours dies, as if by overpopulation.
+     Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+     */
+    @objc func tick() {
+        self.killLivingCells()
+        self.birthNewCells()
+        
+        // Update the UI.
+        for (cell, view) in self.cellToViews {
+            view.backgroundColor = cell.state.color
+        }
+    }
+    
+    /**
+     Death: a cell can die by:
+     - Overcrowding: if a cell is alive at time t + 1 and 4 or more of
+     its neighbors are also alive at time t, the cell will be dead at
+     time t + 1.
+     - Exposure: If a live cell at time t has only 1 live neighbor or no
+     live neighbors, it will be dead at time t + 1.
+     */
+    private func killLivingCells() {
+        let livingCells = self.cells.filter { $0.state == .alive }
+        
+        for liveCell in livingCells {
+            guard let neighbors = self.cellToNeighboringCells[liveCell] else {
+                return
+            }
+            let livingNeighbors = neighbors.filter{ $0.state == .alive }
+            if livingNeighbors.count < 2 || livingNeighbors.count > 3 {
+                liveCell.state = .dead
+            }
+        }
+    }
+    
+    /**
+     Birth: a cell that is dead at time t will be alive at time t + 1
+     if exactly 3 of its eight neighbors were alive at time t.
+     */
+    private func birthNewCells() {
+        let deadCells = self.cells.filter { $0.state == .dead }
+        
+        for deadCell in deadCells {
+            guard let neighbors = self.cellToNeighboringCells[deadCell] else {
+                return
+            }
+            let livingNeighbors = neighbors.filter{ $0.state == .alive }
+            if livingNeighbors.count == 3 {
+                deadCell.state = .alive
             }
         }
     }
@@ -28,25 +100,9 @@ extension World: Renderable {
         let buffer = UIScreen.main.bounds.height - (Cell.Length * CGFloat(World.Dimension))
         view.frame = CGRect(origin: CGPoint(x: 0, y: buffer),
                             size: CGSize(width: length, height: length))
-        for subView in self.cells.values {
+        for subView in self.cellToViews.values {
             view.addSubview(subView)
         }
         return view
-    }
-}
-
-
-extension CGFloat {
-    static func random() -> CGFloat {
-        return CGFloat(arc4random()) / CGFloat(UInt32.max)
-    }
-}
-
-extension UIColor {
-    static var random: UIColor {
-        return UIColor(red:   .random(),
-                       green: .random(),
-                       blue:  .random(),
-                       alpha: 1.0)
     }
 }
