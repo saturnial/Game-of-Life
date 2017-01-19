728d9267b423af93dee86b5bf38e3e95dda29685
Kayvon Tehranian <ktehranian@gmail.com>
2017-01-19 10:33:09 -0500

    Initial Commit

---
+++ Game of Life/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Game of Life
+//
+//  Created by Kayvon Tehranian on 1/19/17.
+//  Copyright © 2017 Loki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
